5586f5ad8c16d896dba88b18ac0483e8a69fccc7
Kittinan Srithaworn <tunxedge@gmail.com>
2017-09-01 22:09:24 +0700

    - implemented some function

---
+++ src/PromptPay.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace KS;
+
+class PromptPay {
+
+  const ID_PAYLOAD_FORMAT = '00';
+  const ID_POI_METHOD = '01';
+  const ID_MERCHANT_INFORMATION_BOT = '29';
+  const ID_TRANSACTION_CURRENCY = '53';
+  const ID_TRANSACTION_AMOUNT = '54';
+  const ID_COUNTRY_CODE = '58';
+  const ID_CRC = '63';
+  
+  const PAYLOAD_FORMAT_EMV_QRCPS_MERCHANT_PRESENTED_MODE = '01';
+  const POI_METHOD_STATIC = '11';
+  const POI_METHOD_DYNAMIC = '12';
+  const MERCHANT_INFORMATION_TEMPLATE_ID_GUID = '00';
+  const BOT_ID_MERCHANT_PHONE_NUMBER = '01';
+  const BOT_ID_MERCHANT_TAX_ID = '02';
+  const GUID_PROMPTPAY = 'A000000677010111';
+  const TRANSACTION_CURRENCY_THB = '764';
+  const COUNTRY_CODE_TH = 'TH';
+
+  public function generatePayload($target, $amount = null) {
+
+    $targetType = strlen($target) >= 13 ? self::BOT_ID_MERCHANT_TAX_ID : self::BOT_ID_MERCHANT_PHONE_NUMBER;
+  }
+
+  public function f($id, $value) {
+    return implode('', [$id, substr('00' + strlen($value), -2), $value]);
+  }
+
+  public function formatTarget($target) {
+
+    $str = str_replace('-', '', $target);
+    $str = preg_replace('/^0/', '66', $str);
+    $str = '0000000000000' . $str;
+
+    return substr($str, -13);
+  }
+
+  public function formatAmount($amount) {
+    return number_format($amount, 2, '.', '');
+  }
+
+  public function crc16($data) {
+    $crc16 = new \mermshaus\CRC\CRC16CCITT();
+    $crc16->update($data);
+    $checksum = $crc16->finish();
+    return bin2hex($checksum);
+  }
+
+}
---
+++ tests/PromptPayTest.php
@@ -0,0 +1,67 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/PromptPay.php';
+
+/**
+ * @property KS\PromptPay PromptPay
+ */
+class PromptPayTest extends PHPUnit_Framework_TestCase {
+
+  private $PromptPay;
+
+  public function __construct() {
+    $this->PromptPay = new KS\PromptPay();
+  }
+
+  public function testFormatTarget() {
+    
+    //Target phone number
+    $target = '0899999999';
+    $result = $this->PromptPay->formatTarget($target);
+    $expected = '0066899999999';
+    $this->assertEquals($expected, $result);
+    
+    $target = '089-999-9999';
+    $result = $this->PromptPay->formatTarget($target);
+    $expected = '0066899999999';
+    $this->assertEquals($expected, $result);
+    
+    //Target ID
+    $target = '1234567890123';
+    $result = $this->PromptPay->formatTarget($target);
+    $expected = '1234567890123';
+    $this->assertEquals($expected, $result);
+    
+  }
+  
+  public function testformatAmount() {
+    
+    $amount = 1337.1337;
+    $result = $this->PromptPay->formatAmount($amount);
+    $expected = '1337.13';
+    $this->assertEquals($expected, $result);
+    
+    $amount = 1337.1387;
+    $result = $this->PromptPay->formatAmount($amount);
+    $expected = '1337.14';
+    $this->assertEquals($expected, $result);
+  }
+  
+  public function testCrc16() {
+    
+    //https://www.blognone.com/node/95133
+    $data = '00020101021129370016A000000677010111011300660000000005802TH53037646304';
+    $result = $this->PromptPay->crc16($data);
+    $expected = '8956';
+    $this->assertEquals($expected, $result);
+  }
+  
+  public function testGeneratePayload() {
+    
+    $target = '0891234567';
+    $result = $this->PromptPay->generatePayload($target);
+    
+  }
+
+}
